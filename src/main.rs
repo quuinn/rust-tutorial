3f7fcf595d319ed2ca0e2de26d52f13c8513cdde
quuinn <kunchuan0903@gmail.com>
2022-06-08 16:02:45 +0800

    refactor: 移除不必要的變數與調整變數名稱

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use std::io;
 
 fn main() {
-    let n: i32;
     println!("[Fibonacci 數列 n 位產生器]");
     loop {
         let mut user_in: String = String::new();
@@ -10,16 +9,18 @@ fn main() {
         let user_in: i32 = user_in.trim().parse().expect("ERR: 請輸入正整數");
 
         if user_in > 0 {
-            n = user_in;
+            println!(
+                "Fibonacci 數列第 {} 位的值為: {}",
+                user_in,
+                fibonacci(user_in, 1, 1)
+            );
             break;
         }
     }
-
-    println!("Fibonacci 數列第 {} 位的值為: {}", n, fibonacci(n, 1, 1));
 }
 fn fibonacci(n: i32, previous: u64, current: u64) -> u64 {
     if n < 2 {
-        previous
+        current
     } else {
         fibonacci(n - 1, current, previous + current)
     }
